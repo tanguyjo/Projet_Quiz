a903de05d7cfbd7bcdc688cf0c8b4edfc8bbb4db
Theo <theobutz.03@gmail.com>
2024-10-28 16:15:42 +0100

    Update

--- game.js
+++ game.js
@@ -8,4 +8,17 @@ const image = document.getElementsByClassName ('img');
 
 const reponses = document.getElementsByClassName ('options');
 
-const button = document.getElementsById ('next-button');
+const button = document.getElementById ('next-button');
+
+
+const myFirstQuestion = quiz_whoPaintThat.questions[1]
+
+question.innerText = myFirstQuestion ;
+
+
+myFirstQuestion.options.forEach(options => {
+    const optionsButton = document.createElement('button');
+    optionsButton.innerText = quiz_whoPaintThat.options;
+    __________.classList.add('__________');
+    __________.appendChild(__________);
+  });

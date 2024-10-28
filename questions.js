8c217b21c06bb622ed1833a494edb0d7157f8d29
tanguyjo <tanguyjonqua@gmail.com>
2024-10-28 12:34:27 +0100

    Creation du squelette du quiz

---
+++ questions.js
@@ -0,0 +1,46 @@
+export const quiz_whoPaintThat = {
+    questions: [
+      {
+        text: "Qui a peint “La nuit étoilée” ?",
+        options: [
+          "Paul Gauguin",
+          "Salvador Dali",
+          "Henri Matisse",
+          "Vincent Van Gogh"
+        ],
+        correct_answer: "Vincent Van Gogh" // https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/2560px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg
+      },
+      {
+        text: "Qui a peint “La persistance de la mémoire” ?",
+        options: [
+          "Paul Gauguin",
+          "Salvador Dali",
+          "Henri Matisse",
+          "Vincent Van Gogh"
+        ],
+        correct_answer: "Salvador Dali" // https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/2560px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg
+      },
+     
+      {
+        text: "Qui a peint “Les nymphéas” ?",
+        options: [
+          "Paul Gauguin",
+          "Salvador Dali",
+          "Claude Monet",
+          "Vincent Van Gogh"
+        ],
+        correct_answer: "Claude Monet" // https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/2560px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg
+      },
+
+      {
+        text: "Qui a peint “La Jeune Fille à la perle” ?",
+        options: [
+          "Paul Gauguin",
+          "Johannes Vermeer",
+          "Henri Matisse",
+          "Vincent Van Gogh"
+        ],
+        correct_answer: "Johannes Vermeer" // https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/2560px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg
+      },
+    ]
+  };

c11110654b3a63c8d52ae42361968cb769f3fd68
Theo <theobutz.03@gmail.com>
2024-10-28 15:09:37 +0100

    creation des variables pour recuperer les elements du DOM

---
+++ game.js
@@ -0,0 +1,11 @@
+import { quiz_whoPaintThat} from './questions.js';
+
+const quiz = document.getElementById ('quiz-container');
+
+const question = document.getElementsByClassName ('question');
+
+const image = document.getElementsByClassName ('img');
+
+const reponses = document.getElementsByClassName ('options');
+
+const button = document.getElementsById ('next-button');

ef391dad8aab124759fddd38cb563c4165903779
tanguyjo <tanguyjonqua@gmail.com>
2024-10-28 14:40:57 +0100

    Commit du index.html et questions.js

--- questions.js
+++ questions.js
@@ -1,46 +1,46 @@
 export const quiz_whoPaintThat = {
     questions: [
       {
-        text: "Qui a peint “La nuit étoilée” ?",
+        text: "Quelle ville est représentée sur le tableau “La nuit étoilée” de Vincent van Gogh ?",
         options: [
-          "Paul Gauguin",
-          "Salvador Dali",
-          "Henri Matisse",
-          "Vincent Van Gogh"
+          "Paris",
+          "Saint-Tropez",
+          "Carcassonne",
+          "Saint-Rémy-de-Provence"
         ],
-        correct_answer: "Vincent Van Gogh" // https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/2560px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg
+        correct_answer: "Saint-Rémy-de-Provence" // https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/2560px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg
       },
       {
-        text: "Qui a peint “La persistance de la mémoire” ?",
+        text: "Comment s’appelle cette toile de Frida Kahlo ?",
         options: [
-          "Paul Gauguin",
-          "Salvador Dali",
-          "Henri Matisse",
-          "Vincent Van Gogh"
+          "Les deux Fridas",
+          "Autoportrait en miroir",
+          "Moi et mon double",
+          "Le fil rouge"
         ],
-        correct_answer: "Salvador Dali" // https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/2560px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg
+        correct_answer: "Les deux Fridas" // https://image.jimcdn.com/app/cms/image/transf/dimension=652x10000:format=jpg/path/s33f4b296991f4cd1/image/i4c78f5fd2a791603/version/1494852367/frida-kahlo-les-deux-frida-1939.jpg
       },
      
       {
         text: "Qui a peint “Les nymphéas” ?",
         options: [
-          "Paul Gauguin",
-          "Salvador Dali",
+          "Henri Matisse",
+          "Edvard Munch",
           "Claude Monet",
-          "Vincent Van Gogh"
+          "Johannes Vermeer"
         ],
         correct_answer: "Claude Monet" // https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/2560px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg
       },
 
       {
-        text: "Qui a peint “La Jeune Fille à la perle” ?",
+        text: "Que représente la perle dans le tableau “La Jeune Fille à la perle” de Johannes Vermeer ?",
         options: [
-          "Paul Gauguin",
-          "Johannes Vermeer",
-          "Henri Matisse",
-          "Vincent Van Gogh"
+          "L’enfance",
+          "La pureté",
+          "La richesse",
+          "Le voyage"
         ],
-        correct_answer: "Johannes Vermeer" // https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/2560px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg
+        correct_answer: "La pureté" // https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/2560px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg
       },
     ]
   };

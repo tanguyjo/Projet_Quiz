a88a784edd2c2611a6c590fa7f66a65c353cd312
alex <alexadkl@gmail.com>
2024-10-29 10:43:29 +0100

    fin étape 4

--- game.js
+++ game.js
@@ -2,23 +2,42 @@ import { quiz_whoPaintThat} from './questions.js';
 
 const quiz = document.getElementById ('quiz-container');
 
-const question = document.getElementsByClassName ('question');
+const titleQuestion = document.getElementById ('question-text');
 
-const image = document.getElementsByClassName ('img');
+const imageQuestion = document.getElementById ('image');
 
-const responses = document.getElementsByClassName ('options');
+const responses = document.getElementById ('options-container');
 
 const button = document.getElementById ('next-button');
 
 
-const myFirstQuestion = quiz_whoPaintThat.questions[0]
+const myFirstQuestion = quiz_whoPaintThat.questions[1]
 
-question.innerText = myFirstQuestion ;
+titleQuestion.innerText = myFirstQuestion.text ;
 
+console.log(titleQuestion)
+console.log(myFirstQuestion)
 
-myFirstQuestion.options.forEach(options => {
+ myFirstQuestion.img.forEach(img => {
     const optionsButton = document.createElement('button');
     optionsButton.innerText = options;
     optionsButton.classList.add('options');
     responses[0].appendChild(optionsButton);
   });
+
+
+myFirstQuestion.options.forEach(options => {
+    const optionsButton = document.createElement('button');
+    optionsButton.innerText = options;
+    optionsButton.classList.add('options-container');
+    responses.appendChild(optionsButton);
+  });
+
+ 
+
+/*   myFirstQuestion.question.forEach(question => {
+    const questionText = document.createElement('string');
+    questionText.innerText = question;
+    questionText.classList.add('question');
+    question[0].appendChild(questionText);
+  }); */

4017c75fc3fdf63d43a3aab322b9a34651e4de87
tanguyjo <tanguyjonqua@gmail.com>
2024-10-29 14:37:22 +0100

    Fin de l'etape 5

--- game.js
+++ game.js
@@ -8,19 +8,55 @@ const imageQuestion = document.getElementById ('image');
 
 const responses = document.getElementById ('options-container');
 
-const button = document.getElementById ('next-button');
+const button = document.getElementById ('nextButton');
 
-const myFirstQuestion = quiz_whoPaintThat.questions[2]
+// const myFirstQuestion = quiz_whoPaintThat.questions[2]
 
-titleQuestion.innerText = myFirstQuestion.text ;
+// titleQuestion.innerText = myFirstQuestion.text ;
 
-imageQuestion.setAttribute("src", myFirstQuestion.image) ;
+// imageQuestion.setAttribute("src", myFirstQuestion.image) ;
 
 
-myFirstQuestion.options.forEach(options => {
+// myFirstQuestion.options.forEach(options => {
+//     const optionsButton = document.createElement('button');
+//     optionsButton.innerText = options;
+//     optionsButton.classList.add('options-container');
+//     responses.appendChild(optionsButton);
+//   });
+
+let currentQuestionIndex = 0
+
+function loadQuestion(){
+  responses.innerHTML =" ";
+  const myFirstQuestion = quiz_whoPaintThat.questions[currentQuestionIndex];
+  titleQuestion.innerText = myFirstQuestion.text;
+
+  imageQuestion.setAttribute("src", myFirstQuestion.image) ;
+  myFirstQuestion.options.forEach(options => {
     const optionsButton = document.createElement('button');
     optionsButton.innerText = options;
     optionsButton.classList.add('options-container');
     responses.appendChild(optionsButton);
   });
+}
+
+
+nextButton.addEventListener('click', ()=> {
+
+  currentQuestionIndex ++ ;
+
+if (currentQuestionIndex < quiz_whoPaintThat.questions.length) {
+  loadQuestion();
+} else {
+  responses.innerHTML =" ";
+  // imageQuestion.innerText ="  ";
+  // .button.style.display ="none";
+  titleQuestion.innerText = "Fin du Quiz";  
+  nextButton.style.display= 'none';
+  imageQuestion.setAttribute("src", " ") 
+}
+
+});
+ loadQuestion()
+
 

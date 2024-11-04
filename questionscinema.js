a929a5e5be120467c2b972235fa6f7e297166bda
tanguyjo <tanguyjonqua@gmail.com>
2024-11-04 17:03:13 +0100

    Ajout du QUiz cinema

---
+++ questionscinema.js
@@ -0,0 +1,52 @@
+
+export const quiz_cinema = {
+    questions: [
+      {
+        text: "Qui a réalisé Scarface?      ",
+        image: image.src='https://www.tap-poitiers.com/wp-content/thumbnails/uploads/2023/09/scarface1-tt-width-1600-height-1067-crop-0-bgcolor-000000.jpg',
+        options: [
+          "Quentin Tarantino",
+    
+          "Steven Spielberg",
+          "Ridley Scott",
+          "Brian De Palma"
+        ],
+        correct_answer: "Brian De Palma" 
+      },
+      {
+        text: "Qui a réalisé Beetlejuice?",
+        image: image.src='https://www.ecrannoir.fr/wp-content/uploads/2024/09/BeetlejuiceBeetlejuice-bloc.jpg',
+        options: [
+          "Tim burton",
+          "Christopher Nolan",
+          "Wes Anderson",
+          "Wes Craven"
+        ],
+        correct_answer: "Tim burton" 
+      },
+     
+      {
+        text: "Qui a réalisé Star Wars- Episode 1?",
+        image: image.src='https://mspfilm.org/wp-content/uploads/2024/04/mspfilm-star_wars_phantom_menace-1920x1080-1.jpg',
+        options: [
+          "Martin Scorsese",
+          "Frank Darabont",
+          "George Lucas",
+          "Tony Kaye"
+        ],
+        correct_answer: "George Lucas" 
+      },
+
+      {
+        text: "Qui a réalisé the Truman Show?",
+        image: image.src='https://metrograph.imgix.net/2023/10/the-truman-show.jpeg?fm=pjpg&ixlib=php-3.3.1',
+        options: [
+          "Mel Gibson",
+          "Peter Weir",
+          "George Miller",
+          "Randal Kleiser"
+        ],
+        correct_answer: "Peter Weir"
+      },
+    ]
+  };

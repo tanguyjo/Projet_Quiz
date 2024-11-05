0dc1fa382c8cb8ecf4c20fecbe1e1de636463ce8
tanguyjo <tanguyjonqua@gmail.com>
2024-11-05 17:06:19 +0100

    tout

--- questions.js
+++ questions.js
@@ -52,4 +52,105 @@ export const quiz_whoPaintThat = {
     ]
   };
 
-  
+  export const quiz_cinema = {
+    questions: [
+      {
+        text: "Qui a réalisé Scarface?      ",
+        image: image.src='https://www.tap-poitiers.com/wp-content/thumbnails/uploads/2023/09/scarface1-tt-width-1600-height-1067-crop-0-bgcolor-000000.jpg',
+        options: [
+          "Quentin Tarantino",
+    
+          "Steven Spielberg",
+          "Ridley Scott",
+          "Brian De Palma"
+        ],
+        correct_answer: "Brian De Palma" 
+      },
+      {
+        text: "Qui a réalisé Beetlejuice?",
+        image: image.src='https://www.ecrannoir.fr/wp-content/uploads/2024/09/BeetlejuiceBeetlejuice-bloc.jpg',
+        options: [
+          "Tim burton",
+          "Christopher Nolan",
+          "Wes Anderson",
+          "Wes Craven"
+        ],
+        correct_answer: "Tim burton" 
+      },
+     
+      {
+        text: "Qui a réalisé Star Wars- Episode 1?",
+        image: image.src='https://mspfilm.org/wp-content/uploads/2024/04/mspfilm-star_wars_phantom_menace-1920x1080-1.jpg',
+        options: [
+          "Martin Scorsese",
+          "Frank Darabont",
+          "George Lucas",
+          "Tony Kaye"
+        ],
+        correct_answer: "George Lucas" 
+      },
+
+      {
+        text: "Qui a réalisé the Truman Show?",
+        image: image.src='https://metrograph.imgix.net/2023/10/the-truman-show.jpeg?fm=pjpg&ixlib=php-3.3.1',
+        options: [
+          "Mel Gibson",
+          "Peter Weir",
+          "George Miller",
+          "Randal Kleiser"
+        ],
+        correct_answer: "Peter Weir"
+      },
+    ]
+  };
+
+  export const quiz_architecture = {
+    questions: [
+      {
+        text: "Dans quelle ville se trouve l’immeuble Torres Blancas",
+        image: image.src='https://live.staticflickr.com/4247/34789412821_ffaa062589_b.jpg',
+        options: [
+          "Barcelone",
+         "Séville",
+          "Valence",
+          "Madrid"
+        ],
+        correct_answer: "Madrid" 
+      },
+      {
+        text: "En quelle année la Sagrada Família a-t-elle commencé sa construction ?",
+        image: image.src='https://www.barcelonayellow.com/images/stories/Sagrada_Familia/sagrada_familia_2023_900x506px.jpg',
+        options: [
+          "1882",
+          "1850",
+          "1910",
+          "1900"
+        ],
+        correct_answer: "1882" 
+      },
+     
+      {
+        text: "Qui est l'architecte du Panthéon à Paris ?",
+        image: image.src='https://www.paris-pantheon.fr/var/cmn_inter/storage/images/_aliases/homepage_banner_webp/7/5/6/4/120534657-1-fre-FR/47bbe4aa3b72-Pantheon-1-002-.webp.webp',
+        options: [
+          "Louis Le Vau",
+          " Jean Nouvel",
+          "Jacques-Germain Soufflot ",
+          "Gustave Eiffel"
+        ],
+        correct_answer: "Jacques-Germain Soufflot " 
+      },
+
+      {
+        text: "Qui a conçu le Burj Khalifa à Dubaï ?",
+        image: image.src='https://res.cloudinary.com/hello-tickets/image/upload/ar_16:9,c_fill,f_auto,q_auto,w_800/v1643925506/ekdhedcerp55jiodv2ef.jpg',
+        options: [
+          "Norman Foster",
+          "Adrian Smith",
+          "Santiago Calatrava",
+          "Frank Gehry"
+        ],
+        correct_answer: "Adrian Smith"
+      },
+    ]
+  };
